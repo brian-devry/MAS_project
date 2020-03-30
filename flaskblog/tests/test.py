f2671791806b00c859312cce01e3937f065baff2
Osama Azam <oazam@my.devry.edu>
2020-03-30 03:56:39 +0000

    Updated test.py, added another unit test

--- flaskblog/tests/test.py
+++ flaskblog/tests/test.py
@@ -3,6 +3,7 @@ import flaskblog.forms as form
 import flaskblog.models as model
 import flaskblog.routes as route
 import unittest
+import json
 
 class FlaskBlogTests(unittest.TestCase):
  #test if registeration form works correctly 
@@ -15,5 +16,13 @@ class FlaskBlogTests(unittest.TestCase):
      )
      self.assertIn(b'Your account has been created, you can now log in.',response.data)
 
-if __name__ == "__main__":
+
+ def test_login(self):
+     tester = app.test_client(self)
+     response = tester.get(
+         '/login', content_type = 'html/text')
+     self.assertEqual(response.status_code, 200)
+
+
+     if __name__ == "__main__":
       unittest.main()
