be1da33a3d8ae14037f612532f8f9d999f6e6141
brian-devry <62269733+brian-devry@users.noreply.github.com>
2020-03-29 21:54:30 -0500

    Moved test folder into package

---
+++ flaskblog/tests/db_test.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+import unittest
+testdir = os.path.dirname(__file__)
+
+import flaskblog
+from flaskblog import db
+
+
+class MyTest(TestCase):
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db'"
+    TESTING = True
+
+    def create_app(self):
+
+        # pass in test configuration
+        return create_app(self)
+
+    def setUp(self):
+
+        db.create_all()
+
+    def tearDown(self):
+
+        db.session.remove()
+        db.drop_all()
